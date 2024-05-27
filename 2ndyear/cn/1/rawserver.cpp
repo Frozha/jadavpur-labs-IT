935a06e9a94ae24ca5a4a71008cafff15bf15027
frozha <wikzander@gmail.com>
2024-05-27 20:34:09 +0530

    raw server client

--- 2ndyear/cn/1/rawserver.cpp
+++ 2ndyear/cn/1/rawserver.cpp
@@ -1,30 +1,60 @@
-// Server.cpp
-
 #include <iostream>
+#include <cstring>
 #include <sys/socket.h>
+#include <netinet/ip.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <cstring>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main() {
-    int serverSocket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
-    if (serverSocket == -1) {
-        
-        std::cerr << "Socket creation failed\n";
-        return -1;
+    int sockfd;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    // Creating raw socket
+    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        return 1;
     }
 
-    char buffer[1024] = {0};
-    sockaddr_in clientAddr;
-    socklen_t clientAddrSize = sizeof(clientAddr);
-    recvfrom(serverSocket, buffer, 1024, 0, (struct sockaddr *)&clientAddr, &clientAddrSize);
-    std::cout << "Received message from client: " << buffer << std::endl;
+    // Set socket option to include IP headers
+    int optval = 1;
+    if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &optval, sizeof(optval)) < 0) {
+        perror("Error setting IP_HDRINCL");
+        return 1;
+    }
 
-    // Sending a message back to the client
-    std::string response = "Hello from server!";
-    sendto(serverSocket, response.c_str(), response.size(), 0, (struct sockaddr *)&clientAddr, clientAddrSize);
+    // Bind the socket to the specified port
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-    close(serverSocket);
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        close(sockfd);
+        return 1;
+    }
 
+    std::cout << "Server listening on port " << PORT << std::endl;
+
+    // Receive data from client
+    while (true) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes_received = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &addr_len);
+        if (bytes_received < 0) {
+            perror("recvfrom failed");
+            close(sockfd);
+            return 1;
+        }
+
+        std::cout << "Received packet from " << inet_ntoa(client_addr.sin_addr) << ": " << buffer << std::endl;
+    }
+
+    close(sockfd);
     return 0;
 }
+
